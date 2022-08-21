d5acda49c3e5c02846bf335df25737b49cf2c948
Dimanchecfa <dimanchenamo@gmail.com>
2022-08-21 18:22:46 +0000

    update

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, View } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
-import RootStack from "./src/router";
 
 import React from "react";
 
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
-    </NativeBaseProvider>
+   
   );
 };
 
